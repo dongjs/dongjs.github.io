49d943263512b6bfe08cdf69bd37616c216cfd6d
Stephen Krewson <stephen.krewson@gmail.com>
2017-06-30 18:09:39 -0400

    don't push just yet; clean up first; good habits

--- scripts/build-dissertation.sh
+++ scripts/build-dissertation.sh
@@ -8,10 +8,11 @@ PROJECT="/mnt/d/stephen-krewson/Documents/StephenKrewson.github.io"
 # for LaTeX and Word when pandoc goes to compile them
 cd $PROJECT/_articles/dissertation
 
-# Loop over all the markdown files
-for f in $(find . -name "*.md")
+# Loop over all the markdown files that have been changed in last hour
+for f in $(find . -mmin -60 -name "*.md")
 do
 	echo $f
 	# render to HTML, PDF, and DOCX
-	$PROJECT/scripts/convert-markdown.sh $f
+	#$PROJECT/scripts/convert-markdown.sh $f
 done
+
