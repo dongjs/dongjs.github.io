3c772eb65c4e5e353ab6343fa4cb0929037db294
Stephen Krewson <stephen.krewson@gmail.com>
2018-01-24 09:53:48 -0500

    ok think that's good for now, still check out stylesheet issue

---
+++ scripts/build-technical.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# absolute path to top level of repo
+PROJECT="/mnt/c/Users/stephen-krewson/Documents/StephenKrewson.github.io"
+
+# move to the dissertation folder, so that image links will resolve properly
+# for LaTeX and Word when pandoc goes to compile them
+cd $PROJECT/_articles/computer-science
+
+# Loop over all the markdown files that have been changed in last hour
+for f in $(find . -mmin -60 -name "*.md")
+do
+	echo $f
+	# render to HTML, PDF, and DOCX
+	$PROJECT/scripts/convert-cs.sh $f
+done
+
